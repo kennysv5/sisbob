1813cdb4fc6f893f274ae505b63f5fdb5906967e
kennysv5 <kennysv_5@hotmail.com>
2017-07-19 08:00:01 -0500

    layoud

---
+++ siscob/src/main/java/com/siscob/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.siscob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String content() {
+        return "home";
+    }
+
+}
